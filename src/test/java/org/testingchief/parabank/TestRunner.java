0da33718d17c9f47189d3b3d4c14e1bf0242c67a
SK <skthetester@gmail.com>
2023-01-27 22:56:25 -0500

    Switch to Junit5

--- src/test/java/org/testingchief/parabank/TestRunner.java
+++ src/test/java/org/testingchief/parabank/TestRunner.java
@@ -1,15 +1,12 @@
-package org.testingchief.parabank;
 
 import com.intuit.karate.junit5.Karate;
 
-import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Karate.class)
-@CucumberOptions(plugin = { "pretty", "html:target/cucumber-html-report",
-        "json:target/cucumber-json-report/cucumber-json.json",
-        "junit:target/cucumber-junit-report" }, features = { "src/test/java/org/testingchief/parabank/" })
-
-public class TestRunner {
+class TestRunner {
+    @Karate.Test
+    Karate testParabankSetup() {
+        return Karate.run("Setup");
+                // .tags("@setup")
+                // .relativeTo(getClass());
+    }
 
 }
