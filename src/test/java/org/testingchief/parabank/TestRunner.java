0ffc1b0f356b04752e986c4b804ad64873e2bc31
SK <skthetester@gmail.com>
2023-01-28 11:55:27 -0500

    Added login and accounts tests

--- src/test/java/org/testingchief/parabank/TestRunner.java
+++ src/test/java/org/testingchief/parabank/TestRunner.java
@@ -3,6 +3,7 @@ package org.testingchief.parabank;
 import com.intuit.karate.junit5.Karate;
 
 class TestRunner {
+
     @Karate.Test
     Karate testParabankSetup() {
         return Karate.run()
@@ -10,4 +11,17 @@ class TestRunner {
          .relativeTo(getClass());
     }
 
+    @Karate.Test
+    Karate testParabankLogin() {
+        return Karate.run()
+                .tags("@parabank_login")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testParabankCustomerAccounts() {
+        return Karate.run()
+                .tags("@parabank_accounts")
+                .relativeTo(getClass());
+    }
 }
