ecb07b2ed6fa20831ecf177246841c5815314254
skthetester <skthetester@gmail.com>
2023-01-28 00:37:57 -0500

    Update testParabankSetup

--- src/test/java/org/testingchief/parabank/TestRunner.java
+++ src/test/java/org/testingchief/parabank/TestRunner.java
@@ -5,9 +5,9 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
     @Karate.Test
     Karate testParabankSetup() {
-        return Karate.run("Setup");
-        // .tags("@setup")
-        // .relativeTo(getClass());
+        return Karate.run()
+         .tags("@parabank_setup")
+         .relativeTo(getClass());
     }
 
 }
