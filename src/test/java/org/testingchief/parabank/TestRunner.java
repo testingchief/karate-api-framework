26a381475e4f57edfd342d448b75aa7935cea5d3
SK <skthetester@gmail.com>
2023-01-29 12:25:29 -0500

    Add cucumber reporting

--- src/test/java/org/testingchief/parabank/TestRunner.java
+++ src/test/java/org/testingchief/parabank/TestRunner.java
@@ -7,42 +7,48 @@ class TestRunner {
     @Karate.Test
     Karate test01_ParabankSetup() {
         return Karate.run()
-         .tags("@parabank_setup")
-         .relativeTo(getClass());
+                .tags("@parabank_setup")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
     @Karate.Test
     Karate test02_ParabankLogin() {
         return Karate.run("login")
-                .relativeTo(getClass());
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
     @Karate.Test
     Karate test03_ParabankCustomerAccounts() {
-        return Karate.run()
-                .tags("@parabank_accounts")
-                .relativeTo(getClass());
+        return Karate.run("classpath:org/testingchief/parabank/accounts.feature")
+                .tags("@parabank_accounts", "~@ignore")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
     @Karate.Test
     Karate test04_ParabankTransactions() {
         return Karate.run()
                 .tags("@parabank_transactions")
-                .relativeTo(getClass());
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
     @Karate.Test
     Karate test05_ParabankCustomers() {
         return Karate.run()
                 .tags("@parabank_customers")
-                .relativeTo(getClass());
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
     @Karate.Test
     Karate test06_ParabankStockPositions() {
         return Karate.run()
-                .tags("@parabank_positions")
-                .relativeTo(getClass());
+                .tags("@parabank_positions", "~@ignore")
+                .relativeTo(getClass())
+                .outputCucumberJson(true);
     }
 
 }
--- src/test/java/org/testingchief/parabank/TestRunnerParallel.java
+++ src/test/java/org/testingchief/parabank/TestRunnerParallel.java
@@ -2,7 +2,15 @@ package org.testingchief.parabank;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +19,22 @@ class TestRunnerParallel {
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:org/testingchief/parabank")
-                .tags()
+                .tags("~@ignore")
+                .outputCucumberJson(true)
                 .parallel(5);
+        generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+    /*
+    Generate Cucumber Report
+     */
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "Parabank REST API");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 }
