85101b57c6852b58c6b758d4918757a5ce4d907c
SK <skthetester@gmail.com>
2023-01-29 10:50:10 -0500

    Rename test runners

--- src/test/java/org/testingchief/parabank/TestRunner.java
+++ src/test/java/org/testingchief/parabank/TestRunner.java
@@ -5,42 +5,41 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
 
     @Karate.Test
-    Karate testParabankSetup() {
+    Karate test01_ParabankSetup() {
         return Karate.run()
          .tags("@parabank_setup")
          .relativeTo(getClass());
     }
 
     @Karate.Test
-    Karate testParabankLogin() {
-        return Karate.run()
-                .tags("@parabank_login")
+    Karate test02_ParabankLogin() {
+        return Karate.run("login")
                 .relativeTo(getClass());
     }
 
     @Karate.Test
-    Karate testParabankCustomerAccounts() {
+    Karate test03_ParabankCustomerAccounts() {
         return Karate.run()
                 .tags("@parabank_accounts")
                 .relativeTo(getClass());
     }
 
     @Karate.Test
-    Karate testParabankTransactions() {
+    Karate test04_ParabankTransactions() {
         return Karate.run()
                 .tags("@parabank_transactions")
                 .relativeTo(getClass());
     }
 
     @Karate.Test
-    Karate testParabankCustomers() {
+    Karate test05_ParabankCustomers() {
         return Karate.run()
                 .tags("@parabank_customers")
                 .relativeTo(getClass());
     }
 
     @Karate.Test
-    Karate testParabankStockPositions() {
+    Karate test06_ParabankStockPositions() {
         return Karate.run()
                 .tags("@parabank_positions")
                 .relativeTo(getClass());
