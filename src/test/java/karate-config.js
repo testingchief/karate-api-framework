41cfa2c8f0d2851d7fcd9c44b831b8c64ce2a53d
skthetester <skthetester@gmail.com>
2023-01-29 18:46:26 -0500

    Parameterize env

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,8 +3,15 @@ function fn() {
     karate.configure('readTimeout', 5000);
     // karate.configure('abortSuiteOnFailure', true);
 
+    var protocol = 'http';
+    var server = '192.168.0.182:8080';
+    if (karate.env == 'prod') {
+        protocol = 'https';
+        server = 'parabank.parasoft.com';
+    }
+
     var config = {
-        baseUrl: 'http://192.168.0.182:8080/parabank/services/bank'
+        baseUrl: protocol + '://' + server + '/parabank/services/bank'
     };
     config.faker = Java.type('com.github.javafaker.Faker');
 
